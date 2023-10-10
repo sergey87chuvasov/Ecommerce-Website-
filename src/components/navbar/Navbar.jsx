f4318138084b4b5a2a68f8956491e766765905de
Sergey87 <serge.techno@gmail.com>
2023-10-11 00:38:14 +0300

    added router in project

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [menu, setMenu] = useState('shop');
@@ -13,21 +14,37 @@ const Navbar = () => {
       </div>
       <ul className='nav-menu'>
         <li onClick={() => setMenu('shop')}>
-          Shop{menu === 'shop' ? <hr /> : <></>}
+          <Link style={{ textDecoration: 'none' }} to='/'>
+            Shop
+          </Link>
+          {menu === 'shop' ? <hr /> : <></>}
         </li>
         <li onClick={() => setMenu('mens')}>
-          Men{menu === 'mens' ? <hr /> : <></>}
+          <Link style={{ textDecoration: 'none' }} to='/mens'>
+            Men
+          </Link>
+          {menu === 'mens' ? <hr /> : <></>}
         </li>
         <li onClick={() => setMenu('womens')}>
-          Women{menu === 'womens' ? <hr /> : <></>}
+          <Link style={{ textDecoration: 'none' }} to='/womens'>
+            Women
+          </Link>
+          {menu === 'womens' ? <hr /> : <></>}
         </li>
         <li onClick={() => setMenu('kids')}>
-          Kids{menu === 'kids' ? <hr /> : <></>}
+          <Link style={{ textDecoration: 'none' }} to='/kids'>
+            Kids
+          </Link>
+          {menu === 'kids' ? <hr /> : <></>}
         </li>
       </ul>
       <div className='nav-login-cart'>
-        <button>Login</button>
-        <img src={cart_icon} alt='cart pic' />
+        <Link to='/login'>
+          <button>Login</button>
+        </Link>
+        <Link to='/cart'>
+          <img src={cart_icon} alt='cart pic' />
+        </Link>
         <div className='nav-cart-count'>0</div>
       </div>
     </div>
