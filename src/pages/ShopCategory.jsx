49d02b9d5b1eaa4bece1295d9d1cdbc6b0090342
Sergey87 <serge.techno@gmail.com>
2023-10-17 00:08:37 +0300

    added createContext in project

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './CSS/ShopCategory.css';
+import { useContext } from 'react';
+import { ShopContext } from '../Context/ShopContext';
 
-const ShopCategory = () => {
-  return <div></div>;
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
+  return (
+    <div className='shop-category'>
+      <img src={props.banner} alt='banners pic' />
+    </div>
+  );
 };
 
 export default ShopCategory;
