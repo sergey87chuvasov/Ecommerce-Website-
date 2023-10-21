e0fcf758845ef1896e98bae976de1caff8f03314
Sergey87 <serge.techno@gmail.com>
2023-10-22 00:31:00 +0300

    fixed cartItems section

--- src/components/CartItems/CartItems.jsx
+++ src/components/CartItems/CartItems.jsx
@@ -21,7 +21,7 @@ const CartItems = () => {
         if (cartItems[e.id] > 0) {
           return (
             <div>
-              <div className='cartitems-format'>
+              <div className='cartitems-format cartitems-format-main'>
                 <img
                   className='carticon-product-icon'
                   src={e.image}
@@ -32,8 +32,9 @@ const CartItems = () => {
                 <button className='cartitems-quantity'>
                   {cartItems[e.id]}
                 </button>
-                <p>{e.new_price * cartItems[e.id]}</p>
+                <p>${e.new_price * cartItems[e.id]}</p>
                 <img
+                  className='cartitems-remove-icon'
                   src={remove_icon}
                   onClick={() => {
                     removeFromCart(e.id);
@@ -45,7 +46,36 @@ const CartItems = () => {
             </div>
           );
         }
+        return null;
       })}
+      <div className='cartitems-down'>
+        <div className='cartitems-total'>
+          <h1>Cart Totals</h1>
+          <div>
+            <div className='cartitems-total-item'></div>
+            <p>Subtotal</p>
+            <p>${0}</p>
+          </div>
+          <hr />
+          <div className='cartitems-total-item'>
+            <p>Shipping Fee</p>
+            <p>Free</p>
+          </div>
+          <hr />
+          <div className='cartitems-total-item'>
+            <h3>Total</h3>
+            <h3>${0}</h3>
+          </div>
+        </div>
+        <button>PROCEED TO CHECKOUT</button>
+        <div className='cartitems-promocode'>
+          <p>If you have a prome code, Enter it here</p>
+          <div className='cartitems-promobox'>
+            <input type='text' placeholder='promo code' />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
