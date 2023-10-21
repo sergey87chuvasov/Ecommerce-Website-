f1ac2fd1088adcb1c539ee6a15bbbb2eb20be00e
Sergey87 <serge.techno@gmail.com>
2023-10-21 23:53:05 +0300

    added CartItems section

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -20,12 +20,12 @@ const ShopContextProvider = (props) => {
 
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-    console.log(cartItems);
+    // console.log(cartItems);
   };
 
   const removeFromCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-    console.log(cartItems);
+    // console.log(cartItems);
   };
 
   const contextValue = { all_product, cartItems, addToCart, removeFromCart };
